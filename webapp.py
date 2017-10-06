a204cb79e4786c11e0958ad11314dd4c8f85c246
calliefausey <31317882+calliefausey@users.noreply.github.com>
2017-10-06 09:39:50 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -15,31 +15,31 @@ def render_page2():
     return render_template('page2.html')
     
 @app.route("/p3")
-def render_page1():
+def render_page3():
     return render_template('page3.html')
 
 @app.route("/p4")
-def render_page2():
+def render_page4():
     return render_template('page4.html')
     
 @app.route("/p5")
-def render_page1():
+def render_page5():
     return render_template('page5.html')
 
 @app.route("/p6")
-def render_page2():
+def render_page6():
     return render_template('page6.html')
     
 @app.route("/p7")
-def render_page2():
+def render_page7():
     return render_template('page7.html')
     
 @app.route("/p8")
-def render_page1():
+def render_page8():
     return render_template('page8.html')
 
 @app.route("/p9")
-def render_page2():
+def render_page9():
     return render_template('page9.html')
     
 if __name__=="__main__":
